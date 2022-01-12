a858fd09509182f443de0c329107430285236a75
HCKME <k.sarvesh1997@gmail.com>
2022-01-12 22:05:42 +0530

    0

--- js/calculator.js
+++ js/calculator.js
@@ -31,24 +31,21 @@ let calculator = () => {
             equation[c] += inputValue;
         }
     }
-    // document.getElementById("footerDiv").innerHTML = equation;
 }
-
 
 //equation has the value
 let display = () => {
+    var resultInternal=0;
     var snd = new Audio("sound/buttonSound/clickSound.wav");
     snd.play();
     for (let star = 0; star < equation.length; star++) {
+        result=equation[star-1];
         if (equation[star] == '\^') {
-            result = parseInt(equation[star-1]);
             for (let index = 1; index < parseInt(equation[star+1]); index++) {
-                result *= equation[star-1];
-                // document.getElementById("footerDiv").innerHTML += ` Result->${result}`;
+                result=result*equation[star-1];
             }
             equation[star-1]=result;
             equation.splice(star,2);
-            // document.getElementById("footerDiv").innerHTML += ` equation->${equation[star-1]}`;
             star=0;
         }
     }
